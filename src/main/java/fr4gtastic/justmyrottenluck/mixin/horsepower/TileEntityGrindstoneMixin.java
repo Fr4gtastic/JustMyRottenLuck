5db2af1e258ee16704c2353a81ea7a91799f2081
Fr4gtastic <milosz.swierad@gmail.com>
2021-05-29 00:37:14 +0200

    use CapabilityFood#updateFoodFromPrevious in HP mixins

--- src/main/java/fr4gtastic/justmyrottenluck/mixin/horsepower/TileEntityGrindstoneMixin.java
+++ src/main/java/fr4gtastic/justmyrottenluck/mixin/horsepower/TileEntityGrindstoneMixin.java
@@ -1,9 +1,6 @@
 package fr4gtastic.justmyrottenluck.mixin.horsepower;
 
-import fr4gtastic.justmyrottenluck.util.RotUtil;
-import net.dries007.tfc.objects.Powder;
-import net.dries007.tfc.objects.items.ItemPowder;
-import net.minecraft.item.ItemFood;
+import net.dries007.tfc.api.capability.food.CapabilityFood;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -42,15 +39,13 @@ public abstract class TileEntityGrindstoneMixin extends TileEntityHPHorseBase {
             ItemStack output = getStackInSlot(1);
             ItemStack secondaryOutput = getStackInSlot(2);
 
-            if (RotUtil.isInputRotten(input) && result.getItem() instanceof ItemFood) {
-                result = new ItemStack(ItemPowder.get(Powder.FERTILIZER));
-                if (secondary.getItem() instanceof ItemFood) {
-                    secondary = new ItemStack(ItemPowder.get(Powder.FERTILIZER));
-                }
-            }
-
             if (output.isEmpty()) {
-                setInventorySlotContents(1, result.copy());
+                ItemStack newOutput = result.copy();
+                if (input.hasCapability(CapabilityFood.CAPABILITY, null))
+                {
+                    CapabilityFood.updateFoodFromPrevious(input, newOutput);
+                }
+                setInventorySlotContents(1, newOutput);
             } else if (output.isItemEqual(result)) {
                 output.grow(result.getCount());
             }
